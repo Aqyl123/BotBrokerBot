afb2f133b81a07de63b5d76fac65db57042b81c8
Aqyl123 <44101248+Aqyl123@users.noreply.github.com>
2020-06-28 17:16:44 -0500

    Add files via upload

---
+++ bots/tohru.py
@@ -0,0 +1,30 @@
+# TOHRUAIO RECENT SALE
+import requests
+import pandas as pd
+from pandas import DataFrame
+
+# LIFETIME
+tohru_url = 'https://www.botbroker.io/bots/17/chart?key_type=lifetime&days=365'
+response = requests.get(tohru_url)
+response.raise_for_status()
+
+try:
+	result = response.json()
+	df = DataFrame(result).sort_values(by=[0], ascending=False).reset_index(drop=True)
+except (IndexError, KeyError):
+	result = None
+
+tohruL = 'No recent sales.' if not result else f'${df[1][0]}'
+
+# RENEWAL
+tohruR_url = 'https://botbroker.io/bots/17/chart?key_type=renewal&days=365'
+response = requests.get(tohruR_url)
+response.raise_for_status()
+
+try:
+	result = response.json()
+	df2 = DataFrame(result).sort_values(by=[0], ascending=False).reset_index(drop=True)
+except (IndexError, KeyError):
+	result = None
+
+tohruR = 'No recent sales.' if not result else f'${df2[1][0]}'
